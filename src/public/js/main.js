aa0fc70fa2985e47c305d1bd5be631c731381ce3
Lean Alvarez <leandro.alvarez8727@gmail.com>
2018-12-21 11:31:46 -0300

    agrego favicon y seccion de ubicacion

---
+++ src/public/js/main.js
@@ -0,0 +1,13 @@
+//smooth scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//smooth scroll
